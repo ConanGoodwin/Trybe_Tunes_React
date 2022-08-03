c3050687cbb39235fb79b70618c0dcd4fbaa4219
ConanGoodwin <conan_goodwin@hotmail.com>
2022-08-03 12:56:23 -0300

    Requisito 07

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      albumInfo: {},
+      albumTracks: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    // const { match: { params: { id } } } = this.props;
+    // const respApi = await getMusics(id);
+
+    // this.setState({
+    //   albumInfo: respApi[0],
+    //   albumTracks: respApi.filter(({ wrapperType }) => wrapperType !== 'collection'),
+    // });
+  }
+
+  render() {
+    const { trackName, previewUrl, trackId, onChangeFavorite } = this.props;
+
+    return (
+      <div>
+        <p>{trackName}</p>
+        <div>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+        </div>
+        <div>
+          <label htmlFor="chkFavorite" data-testid={ `checkbox-music-${trackId}` }>
+            Favorita
+            <input
+              type="checkbox"
+              name={ trackId }
+              id="chkFavorite"
+              onChange={ onChangeFavorite }
+            />
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  onChangeFavorite: PropTypes.func.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,83 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
+// import '../componentsCss/musicCard.css';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      albumInfo: {},
+      albumTracks: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const { match: { params: { id } } } = this.props;
+    const respApi = await getMusics(id);
+    console.log(respApi);
+
+    this.setState({
+      albumInfo: respApi[0],
+      albumTracks: respApi.filter(({ kind }) => kind === 'song'),
+    });
+  }
+
+  handleChange = (objeto, { target }) => {
+    console.log(objeto);
+    console.log(target.checked);
+  }
+
   render() {
+    const { albumInfo, albumTracks } = this.state;
+    console.log(albumTracks);
+
     return (
       <div data-testid="page-album">
         <Header />
+        <section>
+          <div>
+            Artista:
+            {' '}
+            <span data-testid="artist-name">{albumInfo.artistName}</span>
+            {' :|: '}
+          </div>
+          <div>
+            Album:
+            {' '}
+            <span data-testid="album-name">{albumInfo.collectionName}</span>
+          </div>
+        </section>
+        <section>
+          <ul>
+            {(albumTracks.length > 0) && (
+              albumTracks.map((item) => (
+                <li key={ item.trackId }>
+                  <MusicCard
+                    trackName={ item.trackName }
+                    previewUrl={ item.previewUrl }
+                    trackId={ item.trackId }
+                    onChangeFavorite={ (event) => this.handleChange(item, event) }
+                  />
+                </li>
+              ))
+            )}
+          </ul>
+        </section>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
